282601e70964f351a2583d6241dd5e8034dd0eb8
AndresLlinas <andreslli1707@gmail.com>
2020-05-08 21:18:13 -0500

    Creo que ya funca todo, falta probar con jUnit

--- ProyectoRedes/src/main/Controlador.java
+++ ProyectoRedes/src/main/Controlador.java
@@ -4,12 +4,13 @@
 
 package main;
 
-import java.awt.print.Printable;
+import java.math.BigInteger;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -20,7 +21,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 
 public class Controlador implements Initializable {
@@ -43,6 +43,9 @@ public class Controlador implements Initializable {
 	@FXML // fx:id="txtFieldRangoHosts"
 	private TextField txtFieldRangoHosts; // Value injected by FXMLLoader
 
+	@FXML // fx:id="txtFieldRangoHosts2"
+	private TextField txtFieldRangoHosts2; // Value injected by FXMLLoader
+
 	@FXML // fx:id="txtFieldDireccionIP1"
 	private TextField txtFieldDireccionIP2; // Value injected by FXMLLoader
 
@@ -64,22 +67,39 @@ public class Controlador implements Initializable {
 	@FXML // fx:id="txtFieldBitsHosts"
 	private TextField txtFieldBitsHosts; // Value injected by FXMLLoader
 
-	@FXML // fx:id="btnVerLista"
-	private Button btnVerLista; // Value injected by FXMLLoader
-
 	@FXML
 	private ChoiceBox<?> cBoxMascara;
 
 	@FXML
 	private ChoiceBox<?> cBoxMascara2;
 
+	@FXML // fx:id="txtFieldEntrada"
+	private TextField txtFieldEntrada; // Value injected by FXMLLoader
+
+	@FXML // fx:id="cBoxBase"
+	private ChoiceBox<?> cBoxBase; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtFieldDecimal"
+	private TextField txtFieldDecimal; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtFieldBinario"
+	private TextField txtFieldBinario; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtFieldhexa"
+	private TextField txtFieldhexa; // Value injected by FXMLLoader
+
+	@FXML // fx:id="btnConvertir"
+	private Button btnConvertir; // Value injected by FXMLLoader
+
 	@Override
+
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		List<String> list = new ArrayList<String>();
 
 		for (int i = 8; i <= 30; i++) {
 			list.add("/" + i);
 		}
+
 		ObservableList obList = FXCollections.observableList(list);
 		cBoxMascara.getItems().clear();
 		cBoxMascara2.getItems().clear();
@@ -88,17 +108,28 @@ public class Controlador implements Initializable {
 		cBoxMascara.getSelectionModel().select(16);
 		cBoxMascara2.getSelectionModel().select(16);
 
+		List<String> listaBases = new ArrayList<String>();
+
+		listaBases.add("dec");
+		listaBases.add("bin");
+		listaBases.add("hex");
+
+		ObservableList obListBases = FXCollections.observableList(listaBases);
+		cBoxBase.getItems().clear();
+		cBoxBase.setItems(obListBases);
+		cBoxBase.getSelectionModel().selectFirst();
+		;
+
 	}
 
 	public void calcular21() {
 		String ipHost = txtFieldDireccionIP.getText();
-		if (validarIpConMascara(ipHost)) {
+		if (validarFormatoIp(ipHost)) {
 
 			int mascaraSimplificada = Integer
 					.parseInt(cBoxMascara.getSelectionModel().getSelectedItem().toString().substring(1));
 
 			String mascaraDecimal = convertirMascaraSimplificadaADecimal(mascaraSimplificada);
-			System.out.println(mascaraDecimal);
 			String direccionRed = encontrarDireccionRed(ipHost, mascaraDecimal);
 			txtFieldDireccionRed.setText(direccionRed);
 
@@ -108,40 +139,145 @@ public class Controlador implements Initializable {
 			int cantidadHosts = (int) Math.pow(2, bitsHost);
 			txtFieldCantidadHosts.setText(cantidadHosts + "");
 
-			int[] octetosIP = devolverOctetos(direccionRed);
-			int[] octetosMascara = devolverOctetos(direccionBroadcast);
-			txtFieldRangoHosts.setText(octetosIP[0] + "." + octetosIP[1] + "." + octetosIP[2] + "." + (octetosIP[3] + 1)
-					+ " -  " + octetosMascara[0] + "." + octetosMascara[1] + "." + octetosMascara[2] + "."
-					+ (octetosMascara[3] - 1));
+			txtFieldRangoHosts.setText(hallarRango(direccionRed, cantidadHosts));
 
 		} else {
 			Alert alert = new Alert(AlertType.WARNING, "IP incorrecta", ButtonType.OK);
+			alert.setContentText("La dirección ip ingresada no cumple con el formato IPv4");
 			alert.showAndWait();
 		}
 	}
 
 	public void calcular22() {
 		String direccionRed = txtFieldDireccionIP2.getText();
-		if (validarIpConMascara(direccionRed)) {
 
-			int mascaraSimplificada = Integer
-					.parseInt(cBoxMascara2.getSelectionModel().getSelectedItem().toString().substring(1));
+		int mascaraSimplificada = Integer
+				.parseInt(cBoxMascara2.getSelectionModel().getSelectedItem().toString().substring(1));
+		String mascaraDecimal = convertirMascaraSimplificadaADecimal(mascaraSimplificada);
 
-			String mascaraDecimal = convertirMascaraSimplificadaADecimal(mascaraSimplificada);
-			System.out.println(mascaraDecimal);
-			txtFieldMascara.setText(mascaraDecimal);
-			txtFieldBitsRed.setText(mascaraSimplificada + "");
-			int bitsHost = 32 - mascaraSimplificada;
-			txtFieldBitsHosts.setText(bitsHost + "");
-			String direccionBroadcast = encontrarDireccionBroadCast(direccionRed, bitsHost);
-			txtFieldBroadcast2.setText((direccionBroadcast));
-			int cantidadHosts = (int) Math.pow(2, bitsHost);
-			txtFieldCantidadHosts2.setText(cantidadHosts + "");
+		if (validarFormatoIp(direccionRed)) {
+
+			String direccionRedCalculada = encontrarDireccionRed(direccionRed, mascaraDecimal);
+			if (direccionRed.equals(direccionRedCalculada)) {
+
+				txtFieldMascara.setText(mascaraDecimal);
+				txtFieldBitsRed.setText(mascaraSimplificada + "");
+				int bitsHost = 32 - mascaraSimplificada;
+				txtFieldBitsHosts.setText(bitsHost + "");
+				String direccionBroadcast = encontrarDireccionBroadCast(direccionRed, bitsHost);
+				txtFieldBroadcast2.setText((direccionBroadcast));
+				int cantidadHosts = (int) Math.pow(2, bitsHost);
+				txtFieldCantidadHosts2.setText(cantidadHosts + "");
+
+				txtFieldRangoHosts2.setText(hallarRango(direccionRed, cantidadHosts));
+
+			} else {
+				Alert alert = new Alert(AlertType.WARNING, "Direccion IP incorrecta", ButtonType.OK);
+				alert.setContentText(
+						"Verifique que la ip sea una dirección de RED y que corresponda con la mascara seleccionada \n ¿Quiso decir "
+								+ direccionRedCalculada + " ?");
+				alert.showAndWait();
+			}
 
 		} else {
 			Alert alert = new Alert(AlertType.WARNING, "IP incorrecta", ButtonType.OK);
+			alert.setContentText("La dirección ip ingresada no cumple con el formato IPv4");
 			alert.showAndWait();
 		}
+	}
+
+	public void convertir() {
+		String entrada = txtFieldEntrada.getText();
+		switch (cBoxBase.getSelectionModel().getSelectedItem().toString()) {
+		case "dec":
+			if (entrada.matches("[0-9]+")) {
+				txtFieldDecimal.setText(entrada.replaceAll("^0+(?!$)", ""));
+				txtFieldBinario.setText(stringToBinary(entrada));
+				txtFieldhexa.setText(new BigInteger(entrada).toString(16).toUpperCase());
+			} else {
+				Alert alert = new Alert(AlertType.WARNING, "Numero invalido", ButtonType.OK);
+				alert.setContentText("Verifique que haya ingresado un numero decimal");
+				alert.showAndWait();
+			}
+			break;
+
+		case "bin":
+			if (entrada.matches("^[0-1]+$")) {
+				entrada = Integer.parseInt(entrada, 2) + "";
+				txtFieldDecimal.setText(entrada + "");
+				txtFieldBinario.setText(stringToBinary(entrada));
+				txtFieldhexa.setText(new BigInteger(entrada).toString(16).toUpperCase());
+			} else {
+				Alert alert = new Alert(AlertType.WARNING, "Numero invalido", ButtonType.OK);
+				alert.setContentText("Verifique que haya ingresado un numero binario");
+				alert.showAndWait();
+			}
+			break;
+
+		case "hex":
+			if (entrada.matches("[0-9A-F]+")) {
+				BigInteger bigEntrada = new BigInteger(entrada, 16);
+				txtFieldDecimal.setText(bigEntrada + "");
+				txtFieldBinario.setText(bigEntrada.toString(2));
+				txtFieldhexa.setText(new BigInteger(entrada).toString(16).toUpperCase());
+			} else {
+				Alert alert = new Alert(AlertType.WARNING, "Numero invalido", ButtonType.OK);
+				alert.setContentText("Verifique que haya ingresado un numero hexadecimal");
+				alert.showAndWait();
+			}
+			break;
+		}
+	}
+
+	public static String stringToBinary(String text) {
+		String bString = "";
+		String temp = "";
+		for (int i = 0; i < text.length(); i++) {
+			temp = Integer.toBinaryString(text.charAt(i));
+			for (int j = temp.length(); j < 8; j++) {
+				temp = "0" + temp;
+			}
+			bString += temp + " ";
+		}
+		return bString;
+	}
+
+	public boolean isNumeric(String strNum) {
+		Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+		if (strNum == null) {
+			return false;
+		}
+		return pattern.matcher(strNum).matches();
+	}
+
+	private String hallarRango(String ip, int cantidadHosts) {
+
+		String inicioRango = sumarleALaIp(ip, 1);
+		String ipResultado = sumarleALaIp(inicioRango, cantidadHosts - 3);
+
+		return inicioRango + " - " + ipResultado;
+	}
+
+	private String sumarleALaIp(String direccionRed, int cantidad) {
+		int[] octetosIP = devolverOctetos(direccionRed);
+		octetosIP[octetosIP.length - 1] = octetosIP[octetosIP.length - 1] + 1;
+		String inicioRangoBinString = "";
+		for (int octeto : octetosIP) {
+			inicioRangoBinString += rellenarPorIzquierda(Integer.toBinaryString(octeto), 8, '0');
+		}
+		String direccionRedBinaria = sumarBinarios(inicioRangoBinString, Integer.toBinaryString(cantidad - 1));
+		String[] arraybinario = direccionRedBinaria.split("(?<=\\G........)");
+		int[] result = new int[4];
+		for (int i = 0; i < arraybinario.length; i++) {
+			result[i] = Integer.parseInt(arraybinario[i], 2);
+		}
+		return String.join(".", intArrayToStringArray(result));
+	}
+
+	private String[] intArrayToStringArray(int[] intArray) {
+		String str = Arrays.toString(intArray).replaceAll("\\s+", "");
+		String strArray[] = str.substring(1, str.length() - 1).split(",");
+		return strArray;
 	}
 
 	private int[] devolverOctetos(String ip) {
@@ -162,7 +298,7 @@ public class Controlador implements Initializable {
 		for (int i = octetosNumericos.length - 1; i >= 0; i--) {
 
 			String octetoBinario = Integer.toBinaryString(octetosNumericos[i]);
-			octetoBinario = padLeft(octetoBinario, 8, '0');
+			octetoBinario = rellenarPorIzquierda(octetoBinario, 8, '0');
 			String nuevoOctetoBinario = "";
 			for (int j = octetoBinario.length() - 1; j >= 0; j--) {
 
@@ -206,17 +342,31 @@ public class Controlador implements Initializable {
 		return mascara;
 	}
 
-	private static boolean validarIpConMascara(final String ip) {
+	private static boolean validarFormatoIp(final String ip) {
 		String patron = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
 		return ip.matches(patron);
 	}
 
-	public static String padLeft(String str, int n, char replacement) {
+	public static String rellenarPorIzquierda(String str, int n, char replacement) {
 		return String.format("%1$" + n + "s", str).replace(' ', replacement);
 	}
 
-	public static String replaceLast(String text, String regex, String replacement) {
-		return text.replaceFirst("(?s)(.*)" + regex, "$1" + replacement);
-	}
+	static String sumarBinarios(String a, String b) {
 
+		String resultado = "";
+		int s = 0;
+		int i = a.length() - 1, j = b.length() - 1;
+		while (i >= 0 || j >= 0 || s == 1) {
+
+			s += ((i >= 0) ? a.charAt(i) - '0' : 0);
+			s += ((j >= 0) ? b.charAt(j) - '0' : 0);
+
+			resultado = (char) (s % 2 + '0') + resultado;
+
+			s /= 2;
+			i--;
+			j--;
+		}
+		return resultado;
+	}
 }
